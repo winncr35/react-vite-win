4532f9e594e44141a54fbc2386df60a4d2cb03c3
winncr35 <quycr02@gmail.com>
2025-06-09 13:54:41 -0500

    video 30.1

--- src/components/learn/MyComponent.jsx
+++ src/components/learn/MyComponent.jsx
@@ -1,8 +1,16 @@
 //() =>{}
 // component = html + css + js
+// JSX: 1 parent
+import './style.css';
+
 const MyComponent = () => {
     return (
-        <div> eric & hoi dan it update </div>
+        <>
+            <div className=""> eric & hoi dan it update </div>
+            <div className="child"
+                style={{ borderRadius: "10px" }}
+            >child</div>
+        </>
     );
 }
 
