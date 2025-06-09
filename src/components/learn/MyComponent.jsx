60ae595a679c54684c303178c78ae73ab78742e2
winncr35 <quycr02@gmail.com>
2025-06-09 14:21:52 -0500

    video 31

--- src/components/learn/MyComponent.jsx
+++ src/components/learn/MyComponent.jsx
@@ -4,9 +4,19 @@
 import './style.css';
 
 const MyComponent = () => {
+    // const hoidanit = "eric ";// string
+    // const hoidanit = 25;
+    // const hoidanit = true; //boolean
+    const hoidanit = [1, 2, 3];
+    // const hoidanit = {           In dang chuoi JSON
+    //     name: "hoidanit",    In dang chuoi JSON
+    //     age: 25
+
+    // }
     return (
         <>
-            <div className=""> eric & hoi dan it update </div>
+            <div> {JSON.stringify(hoidanit)} & hoi dan it update</div>
+            <div>{console.log("Eric")}</div>
             <div className="child"
                 style={{ borderRadius: "10px" }}
             >child</div>
