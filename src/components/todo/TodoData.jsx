208334685bfb07469f032787afd1c75b667ff033
winncr35 <quycr02@gmail.com>
2025-06-09 20:54:42 -0500

    video 34

---
+++ src/components/todo/TodoData.jsx
@@ -0,0 +1,22 @@
+
+const TodoData = (props) => {
+    // props là một object{}
+    // {
+    //     name :"win",
+    //     age: 25,
+    //     data:{}
+    // }
+    // objec destructoring
+    //  lấy key của object props ra xài
+    const { name, age, data } = props;
+    console.log("Check props: ", props);
+
+    return (
+        <div className='todo-data1'>
+            <div>My name is {name}</div>
+            <div> Learning React</div>
+            <div> Watching Youtube</div>
+        </div>
+    )
+}
+export default TodoData;
---
+++ src/components/todo/TodoNew.jsx
@@ -0,0 +1,11 @@
+const TodoNew = () => {
+
+
+    return (
+        <div className='todo-new'>
+            <input type="text" />
+            <button>Add</button>
+        </div>
+    )
+}
+export default TodoNew;
