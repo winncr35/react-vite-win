f6b0618fa0e64f3282e254960c0395237924c82d
winncr35 <quycr02@gmail.com>
2025-06-11 12:52:21 -0500

    video 38

--- src/App.jsx
+++ src/App.jsx
@@ -16,8 +16,18 @@ const App = () => {
     country: "vietnam"
   }
   const addNewTodo = (name) => {
-    alert(`call me ${name}`)
+    const newTodo = {
+      id: randomIntFromInterval(1, 1000000),
+      name: name
+    }
+
+    setTodoList([...todoList, newTodo])
   }
+
+  const randomIntFromInterval = (min, max) => { // min and max included 
+    return Math.floor(Math.random() * (max - min + 1) + min);
+  }
+
   return (
     <div className="todo-container">
       <div className="todo-title">Todo List</div>
--- src/components/todo/TodoNew.jsx
+++ src/components/todo/TodoNew.jsx
@@ -7,7 +7,7 @@ const TodoNew = (props) => {
     const { addNewTodo } = props;
     // addNewTodo("win");
     const handleClick = () => {
-        console.log("check valueInput: ", valueInput)
+        addNewTodo(valueInput)
     }
 
     const handleOnChange = (name) => {
